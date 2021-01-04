826345bc48a1b1e0b8f126bb36d2ce987383be70
Angela <69411622+Kirbachu88@users.noreply.github.com>
2021-01-04 04:12:33 -0800

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,53 @@ import discord
 import os
 from discord.ext import commands
 
-client = commands.Bot(command_prefix='b!')
+def get_prefix(bot, message):
+    prefixes = ['b! ', 'b!', 'B! ', 'B!']
+
+    if not message.guild:
+        return 'b!'
+
+    return commands.when_mentioned_or(*prefixes)(bot, message)
+
+
+client = commands.Bot(command_prefix=get_prefix, case_insensitive=True)
+
+
+class CustomHelp(commands.MinimalHelpCommand):
+    async def send_pages(self):
+        destination = self.get_destination()
+        for page in self.paginator.pages:
+            embed = discord.Embed(
+                title='A very helpful embed!',
+                description=page,
+                color=discord.Color.gold()
+            )
+            await destination.send(embed=embed)
+
+
+attributes = {
+    'aliases': ['halp'],
+    'cooldown': commands.Cooldown(2, 7.0, commands.BucketType.user)
+}
+
+client.help_command = CustomHelp(command_attrs=attributes)
 
 
 def is_owner(ctx):
     return ctx.author.id == 'Your Discord ID'
+
+
+@client.event
+async def on_command_error(ctx, error):
+    error = getattr(error, "original", error)
+    if isinstance(error, commands.ExtensionNotFound):
+        error = 'Couldn\'t find that!'
+    elif isinstance(error, commands.ExtensionAlreadyLoaded):
+        error = 'Already loaded that!'
+    elif isinstance(error, commands.CommandOnCooldown):
+        await ctx.send(f'You\'re going too fast! (Wait {str(error)[-5:]})', delete_after=5)
+        return
+    await ctx.send(error)
 
 
 @client.command()
