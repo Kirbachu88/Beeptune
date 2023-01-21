a8cb58d739bd8025d347d729127f1f1c4bbdd96e
Angela <69411622+Kirbachu88@users.noreply.github.com>
2023-01-21 07:35:19 -0800

    Renewing bot to discord.py 2.1.0
    Remove guild.region

--- cogs/info.py
+++ cogs/info.py
@@ -2,30 +2,34 @@ import discord
 from discord.ext import commands
 
 
-class Info(commands.Cog):
+class Info(commands.Cog, name='info'):
+    def __init__(self, bot):
+        self.client = bot
 
-    def __init__(self, client):
-        self.client = client
+    # New async cog_load special method is automatically called
+    async def cog_load(self):
+        print("Info: Loaded.")
 
     # Events
     @commands.Cog.listener()
     async def on_ready(self):
-        print("Info: Loaded.")
+        print("Info: Ready!")
 
     # Commands
-    @commands.command()
+    @commands.command(name='ping')
     async def ping(self, ctx):
+        print("lol")
         await ctx.send(f'Pong! {round(self.client.latency * 1000)}ms')
 
-    @commands.command()
+    @commands.command(name='marco')
     async def marco(self, ctx):
         await ctx.send(f'Polo! {round(self.client.latency * 1000)}ms')
 
-    @commands.command()
+    @commands.command(name='info')
     async def info(self, ctx):
         await ctx.send("Descriptive words")
 
-    @commands.command()
+    @commands.command(name='server')
     async def server(self, ctx):
         name = str(ctx.guild.name)
         description = str(ctx.guild.description)
@@ -34,10 +38,9 @@ class Info(commands.Cog):
 
         owner = str(ctx.guild.owner)
         server_id = str(ctx.guild.id)
-        region = str(ctx.guild.region)
         member_count = str(ctx.guild.member_count)
 
-        icon = str(ctx.guild.icon_url)
+        icon = str(ctx.guild.icon.url)
 
         # Creating a fancy embed
         embed = discord.Embed(
@@ -48,11 +51,10 @@ class Info(commands.Cog):
         embed.set_thumbnail(url=icon)
         embed.add_field(name="Owner", value=owner, inline=True)
         embed.add_field(name="Server ID", value=server_id, inline=True)
-        embed.add_field(name="Region", value=region, inline=True)
         embed.add_field(name="Member Count", value=member_count, inline=True)
 
         await ctx.send(embed=embed)
 
 
-def setup(client):
-    client.add_cog(Info(client))
+async def setup(bot):
+    await bot.add_cog(Info(bot))
